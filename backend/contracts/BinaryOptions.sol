ddbe257281fe1e2ba5ea183248fd5648f7ee7659
HoangLe87 <hongacek@hotmail.com>
2023-01-14 00:28:51 +0100

    binaryoptions init

--- backend/contracts/BinaryOptions.sol
+++ backend/contracts/BinaryOptions.sol
@@ -9,18 +9,24 @@ contract BinaryOptions {
     AggregatorV3Interface internal priceFeed;
     IERC20 public ANNA;
     IERC20 public LOVE;
-    uint public expiry30min = 3600;
-    uint public expiry30min = 1800;
-    uint public expiry15min = 900;
-    uint public startTime = ;
-    uint public endTime = ;
-    bool public inProgess;
-    mapping(address=>mapping(bool=>uint256)) adrToOutcomeToStaked;
-    uint totalStakedTrue;
-    uint totalStakedFalse;
+    struct Player {
+        bool madeABet;
+        bool prediction;
+        uint256 betAmount;
+    }
+    uint256 public expiry60min = 3600;
+    uint256 public expiry30min = 1800;
+    uint256 public expiry15min = 900;
+    mapping(address => Player) public players;
 
+    uint256 public startTime;
+    uint256 public endTime;
+    bool public inProgress;
+    uint256 totalTrue;
+    uint256 totalFalse;
 
-    event Bet(address, uint256, bool)
+    event Bet(address, uint256, bool);
+
     /**
      * Network: Goerli
      * Aggregator: ETH/USD
@@ -30,41 +36,65 @@ contract BinaryOptions {
         priceFeed = AggregatorV3Interface(
             0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
         );
-        ANNA = IERC(_annaAddress);
-        LOVE = IERC(_loveAddress);
+        ANNA = IERC20(_annaAddress);
+        LOVE = IERC20(_loveAddress);
     }
+
     /**
      * Returns the latest price
      */
-    function getLatestPrice() public view returns (int) {
-        (
-            ,
-            /*uint80 roundID*/ int price /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/,
-            ,
-            ,
 
-        ) = priceFeed.latestRoundData();
+    function getHistoricalPrice(uint80 roundId) public view returns (int256) {
+        (
+            uint80 id,
+            int256 price,
+            uint256 startedAt,
+            uint256 timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.getRoundData(roundId);
+        require(timeStamp > 0, "Round not complete");
         return price;
     }
 
-    function outcome() private returns (bool) {
-        if 
-    } 
-
     function bet(uint256 _amount, bool _prediction) external {
-        require(!inProgess && block.timestamp<endTime, "Unable to place bets now")
-        require(ANNA.balanceOf(msg.sender)>=_amount0 && _amount>0, "Insufficient funds");
+        require(
+            players[msg.sender].madeABet == false &&
+                block.timestamp < (startTime + 15 minutes),
+            "Unable to place bets now"
+        );
+        require(
+            ANNA.balanceOf(msg.sender) >= _amount && _amount > 0,
+            "Insufficient funds"
+        );
         ANNA.transferFrom(msg.sender, address(this), _amount);
-        if (_prediction==true) {
-            totalStakedTrue+=_amount
-        } else if (_prediction==false) {
-            totalStakedFalse+=_amount
+        players[msg.sender].madeABet = true;
+        players[msg.sender].betAmount = _amount;
+        players[msg.sender].prediction = _prediction;
+        if (_prediction == false) {
+            totalFalse += _amount;
+            if (
+                totalTrue > 1 * 10**18 &&
+                !inProgress &&
+                (((totalTrue * 100) / totalFalse) >= 50)
+            ) {
+                startTime = block.timestamp;
+                endTime = block.timestamp + 30 minutes;
+                inProgress = true;
+            }
+        } else if (_prediction == true && !inProgress) {
+            totalTrue += _amount;
+            if (
+                totalFalse > 1 * 10**18 &&
+                !inProgress &&
+                (((totalFalse * 100) / totalTrue) >= 50)
+            ) {
+                startTime = block.timestamp;
+                endTime = block.timestamp + 30 minutes;
+                inProgress = true;
+            }
         }
-        adrToOutcomeToStaked[msg.sender][_prediction]+=totalStaked;
-        emit Bet(msg.sender, _amount, _prediction)
+        emit Bet(msg.sender, _amount, _prediction);
     }
 
-    function resolve() private{
-
-    } 
+    function resolve() private {}
 }
