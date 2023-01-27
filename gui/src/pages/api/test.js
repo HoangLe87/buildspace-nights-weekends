6433b9f9fd8a508df7ed1834538d8d520aa78672
HoangLe87 <hongacek@hotmail.com>
2023-01-27 18:57:01 +0100

    binance init

--- gui/src/pages/api/test.js
+++ gui/src/pages/api/test.js
@@ -1,11 +1,41 @@
-import Binance from 'binance-api-node'
+import axios from 'axios'
+const Binance = require('binance-api-node')
 
 export default async function Home(req, res) {
   try {
-    const client = Binance()
-
-    const BTC = await client.prices() // get all prices
-    return res.status(200).json(BTC)
+    const prices = await axios.get(
+      'https://api.binance.com/api/v3/ticker/price/symbol=BTCUSDT',
+      {
+        symbol: 'BTCUSDT',
+      }
+    )
+    if (req.method === 'GET') {
+      return res.status(200).json(prices.data)
+    }
+    if (req.method === 'POST') {
+      const amount = req.body.amount
+      const action = req.body.action
+      const token = req.body.token
+      const { adminDb, adminAuth } = initializeFirebaseServer()
+      const decodedToken = await adminAuth.verifyIdToken(token)
+      const email = decodedToken.email
+      console.log('decodedToken', decodedToken)
+      const { db, auth } = initializeFirebaseClient()
+      const usersRef = doc(db, 'Users', email)
+      console.log('2')
+      getDoc(usersRef).then((doc) => {
+        if (doc.exists()) {
+          setDoc(
+            usersRef,
+            {
+              BTCUSDT: amount,
+            },
+            { merge: true }
+          )
+        }
+      })
+      return res.status(200).json('POST')
+    }
   } catch (error) {
     return res.status(400).json(error)
   }
