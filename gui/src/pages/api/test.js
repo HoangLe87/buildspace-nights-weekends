51e83a6ad1543d5b52b53d1f5077c98a6a83493d
HoangLe87 <hongacek@hotmail.com>
2023-01-27 17:49:02 +0100

    binance api init

--- gui/src/pages/api/test.js
+++ gui/src/pages/api/test.js
@@ -1,10 +1,11 @@
-
+import Binance from 'binance-api-node'
 
 export default async function Home(req, res) {
   try {
-    if (req.method === 'GET') {
-      return res.status(200).json('this works')
-    }
+    const client = Binance()
+
+    const BTC = await client.prices() // get all prices
+    return res.status(200).json(BTC)
   } catch (error) {
     return res.status(400).json(error)
   }
