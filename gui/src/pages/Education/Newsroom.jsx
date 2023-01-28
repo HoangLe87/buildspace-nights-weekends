26dcc290d83a1e6e4621834a1ae83f11b0e3e569
HoangLe87 <hongacek@hotmail.com>
2023-01-28 16:59:48 +0100

    news done

--- gui/src/pages/Education/Newsroom.jsx
+++ gui/src/pages/Education/Newsroom.jsx
@@ -4,34 +4,29 @@ import { Header } from '@/components/layout/Header'
 import { EducationNavBar } from '@/components/layout/EducationNavBar'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import Image from 'next/image'
 
 export default function Newsroom() {
   const [trending, setTrending] = useState()
+  const [open, setOpen] = useState(false)
+  const now = Date.now()
+  const fifteenDaysAgo = now - 1296000000
   const options = {
     method: 'GET',
-    url: 'https://finance-social-sentiment-for-twitter-and-stocktwits.p.rapidapi.com/get-social-list',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-RapidAPI-Key': `${process.env.NEWS_API_KEY}`,
-      'X-RapidAPI-Host': `${process.env.NEWS_API_HOST}`,
-    },
+    url: `https://api.coinstats.app/public/v1/news?skip=0&limit=20&toDate=${now}&fromDate=${fifteenDaysAgo}`,
   }
 
   const fetchAllNews = async () => {
     axios
       .request(options)
       .then(function (response) {
-        setTrending({
-          twitter: response.data.twitter,
-          stocktwits: response.data.stocktwits,
-          reddit: response.data.reddit,
-        })
+        setTrending(response.data.news)
+        console.log('news', response.data.news)
       })
       .catch(function (error) {
         console.error(error)
       })
   }
-
   useEffect(() => {
     fetchAllNews()
   }, [])
@@ -48,28 +43,33 @@ export default function Newsroom() {
       <Header currentPage={'Education'} />
       <main className="grid h-screen overflow-hidden bg-[url('../images/education/11.jpeg')] bg-cover">
         <EducationNavBar currentPage={'Newsroom'} />
-        <div className=" mt-10 h-full overflow-auto bg-gray-700/80">
-          <h1 className="mt-10 text-center text-3xl text-white">
-            Tickers trending on socials
-          </h1>
-          <div className="flex justify-center gap-2 overflow-auto text-white">
-            <div className="grid">
-              <h1 className="text-xl">Twitter</h1>
-              {trending && trending.twitter.map((i) => <div key={i}>{i}</div>)}
+        <div className="mt-5 grid overflow-hidden bg-gray-800 p-5 align-middle">
+          {trending?.map((i) => (
+            <div
+              key={i.id}
+              href={i.link}
+              className="flex w-full justify-center gap-4  px-2 text-white hover:cursor-pointer hover:text-slate-300"
+            >
+              <h1>{i.title}</h1>
             </div>
-            <div className="grid">
-              <h1 className="text-xl">Stocktwits</h1>
-              {trending &&
-                trending.stocktwits.map((i) => <div key={i}>{i}</div>)}
-            </div>
-            <div className="grid">
-              <h1 className="text-xl">Reddit</h1>
-              {trending && trending.reddit.map((i) => <div key={i}>{i}</div>)}
-            </div>
-          </div>
+          ))}
         </div>
       </main>
       <Footer />
+    </>
+  )
+}
+
+export const Card = (title, source, description, imgURL, feedDate) => {
+  return (
+    <>
+      <div className="z-1 absolute bg-gray-700 text-white">
+        <Image src={imgURL} width={200} height={200} alt="" />
+        <h1>{title}</h1>
+        <p>Description: {description}</p>
+        <p>Source: {source}</p>
+        <p>Updated: {new Date(feedDate)}</p>
+      </div>
     </>
   )
 }
