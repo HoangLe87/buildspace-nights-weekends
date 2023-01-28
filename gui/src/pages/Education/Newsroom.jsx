ab8abee2fb24954fc2258f71a68b92d0a353e648
HoangLe87 <hongacek@hotmail.com>
2023-01-28 16:06:20 +0100

    tickers

--- gui/src/pages/Education/Newsroom.jsx
+++ gui/src/pages/Education/Newsroom.jsx
@@ -1,20 +1,19 @@
 import Head from 'next/head'
 import { Footer } from '@/components/layout/Footer'
 import { Header } from '@/components/layout/Header'
-import { TypeAnimation } from 'react-type-animation'
 import { EducationNavBar } from '@/components/layout/EducationNavBar'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 export default function Newsroom() {
-  const [news, setNews] = useState()
-
+  const [trending, setTrending] = useState()
   const options = {
     method: 'GET',
-    url: 'https://pro-api.coingecko.com/api/v3/coins/markets',
+    url: 'https://finance-social-sentiment-for-twitter-and-stocktwits.p.rapidapi.com/get-social-list',
     headers: {
-      'X-RapidAPI-Key': '3e1eac0d6cmshae458c659b6ce51p191953jsndd77894067d7',
-      'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com',
+      'Content-Type': 'application/json',
+      'X-RapidAPI-Key': `${process.env.NEWS_API_KEY}`,
+      'X-RapidAPI-Host': `${process.env.NEWS_API_HOST}`,
     },
   }
 
@@ -22,9 +21,11 @@ export default function Newsroom() {
     axios
       .request(options)
       .then(function (response) {
-        console.log('respo', response.data)
-        setNews(response.data)
-        console.log('news', news)
+        setTrending({
+          twitter: response.data.twitter,
+          stocktwits: response.data.stocktwits,
+          reddit: response.data.reddit,
+        })
       })
       .catch(function (error) {
         console.error(error)
@@ -45,20 +46,27 @@ export default function Newsroom() {
         />
       </Head>
       <Header currentPage={'Education'} />
-      <main className="grid h-screen bg-[url('../images/education/11.jpeg')] bg-cover">
+      <main className="grid h-screen overflow-hidden bg-[url('../images/education/11.jpeg')] bg-cover">
         <EducationNavBar currentPage={'Newsroom'} />
-
-        <div className="flex  justify-center bg-gray-700/80 text-center align-middle text-slate-100">
-          <TypeAnimation
-            sequence={[
-              'Comming soon in spring 2023.',
-              1000,
-              'Comming soon in spring 2023. Please check our Roadmap on the Home page',
-            ]}
-            wrapper="div"
-            cursor={true}
-            style={{ fontSize: '2em' }}
-          />
+        <div className=" mt-10 h-full overflow-auto bg-gray-700/80">
+          <h1 className="mt-10 text-center text-3xl text-white">
+            Tickers trending on socials
+          </h1>
+          <div className="flex justify-center gap-2 overflow-auto text-white">
+            <div className="grid">
+              <h1 className="text-xl">Twitter</h1>
+              {trending && trending.twitter.map((i) => <div key={i}>{i}</div>)}
+            </div>
+            <div className="grid">
+              <h1 className="text-xl">Stocktwits</h1>
+              {trending &&
+                trending.stocktwits.map((i) => <div key={i}>{i}</div>)}
+            </div>
+            <div className="grid">
+              <h1 className="text-xl">Reddit</h1>
+              {trending && trending.reddit.map((i) => <div key={i}>{i}</div>)}
+            </div>
+          </div>
         </div>
       </main>
       <Footer />
