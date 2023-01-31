afa7637bd9f371cac2134841ac2a8137e7a38d8f
HoangLe87 <hongacek@hotmail.com>
2023-01-31 14:39:33 +0100

    fear and greed index

--- gui/src/pages/Education/Trending.jsx
+++ gui/src/pages/Education/Trending.jsx
@@ -4,12 +4,12 @@ import { Header } from '@/components/layout/Header'
 import { EducationNavBar } from '@/components/layout/EducationNavBar'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import Image from 'next/image'
 import { FaTwitterSquare } from 'react-icons/fa'
 import { MdOutlineRequestPage } from 'react-icons/md'
 
 export default function Newsroom() {
   const [trending, setTrending] = useState()
+  const [fearIndex, setFearIndex] = useState()
   const options = {
     method: 'GET',
     url: `https://api.coinstats.app/public/v1/coins?skip=0&limit=10&currency=USD`,
@@ -20,14 +20,35 @@ export default function Newsroom() {
       .request(options)
       .then(function (response) {
         setTrending(response.data.coins)
-        console.log('news', response.data.coins)
       })
       .catch(function (error) {
         console.error(error)
       })
   }
+
+  const optionsIndex = {
+    method: 'GET',
+    url: process.env.NEXT_PUBLIC_RAPID_FANDG_URL,
+    headers: {
+      'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_FANDG_KEY,
+      'X-RapidAPI-Host': process.env.NEXT_PUBLIC_RAPID_FANDG_HOST,
+    },
+  }
+  const fetchFearIndex = async () => {
+    axios
+      .request(optionsIndex)
+      .then(function (response) {
+        setFearIndex(response.data.fgi)
+        console.log('index', response.data)
+      })
+      .catch(function (error) {
+        console.error(error)
+      })
+  }
+
   useEffect(() => {
     fetchAllNews()
+    fetchFearIndex()
   }, [])
 
   return (
@@ -43,42 +64,78 @@ export default function Newsroom() {
       <main className="grid min-h-screen overflow-hidden bg-[url('../images/education/11.jpeg')] bg-cover">
         <EducationNavBar currentPage={'Trending'} />
 
-        <div className="m-auto mt-5 grid grid-cols-2 justify-center gap-4 overflow-hidden bg-gray-800/80  p-5 align-middle md:gap-8 lg:grid-cols-5">
-          {trending?.map((i) => (
-            <div
-              key={i.id}
-              className="flex w-full flex-col justify-center px-2 align-middle  text-xs text-white hover:cursor-pointer hover:text-slate-300"
-            >
-              <div className="flex justify-start gap-2 align-middle">
-                <img src={i.icon} alt="icon" className="h-4 w-4" />
-                <h1 className="text-xl">{i.symbol}</h1>
-                <div className="flex justify-center gap-2">
-                  <a className="hover:cursor-pointer" href={i.websiteUrl}>
-                    <MdOutlineRequestPage />
-                  </a>
-                  <a className="hover:cursor-pointer" href={i.twitterUrl}>
-                    <FaTwitterSquare />
-                  </a>
+        <div className="overflow-hidden bg-gray-800/80">
+          {fearIndex && (
+            <div className="text-center text-white">
+              <h1>Fear and Greed index</h1>
+              <div className="flex justify-center gap-4">
+                <div className="text-center text-xs">
+                  <div>Now</div>
+                  <div>
+                    {fearIndex.now.value} {fearIndex.now.valueText}
+                  </div>
+                </div>
+                <div className="text-center text-xs">
+                  <div>Week Ago</div>
+                  <div>
+                    {fearIndex.oneWeekAgo.value}{' '}
+                    {fearIndex.oneWeekAgo.valueText}
+                  </div>
+                </div>
+                <div className="text-center text-xs">
+                  <div>Month Ago</div>
+                  <div>
+                    {fearIndex.oneMonthAgo.value}{' '}
+                    {fearIndex.oneMonthAgo.valueText}
+                  </div>
+                </div>
+                <div className="text-center text-xs">
+                  <div>Year Ago</div>
+                  <div>
+                    {fearIndex.oneYearAgo.value}{' '}
+                    {fearIndex.oneYearAgo.valueText}
+                  </div>
                 </div>
               </div>
-              <div className="flex justify-start gap-2 align-middle">
-                <div>Price: {i.price.toFixed(2)}</div>
-                <div>
-                  {i.priceChange1w > 0 ? (
-                    <div className="text-green-500">
-                      {i.priceChange1w.toFixed(2)}
-                    </div>
-                  ) : (
-                    <div className="text-red-500">
-                      {i.priceChange1w.toFixed(2)}
-                    </div>
-                  )}
-                </div>
-              </div>
-              <div>Mcap: {(i.marketCap / 1000000000).toFixed(2)} b</div>
-              <div>Vol: {(i.volume / 1000000000).toFixed(2)} b</div>
             </div>
-          ))}
+          )}
+          <div className="m-auto mt-5 grid grid-cols-2 justify-center gap-4 overflow-hidden p-5 align-middle md:gap-8 lg:grid-cols-5">
+            {trending?.map((i) => (
+              <div
+                key={i.id}
+                className="flex w-full flex-col justify-center px-2 align-middle  text-xs text-white hover:cursor-pointer hover:text-slate-300"
+              >
+                <div className="flex justify-start gap-2 align-middle">
+                  <img src={i.icon} alt="icon" className="h-4 w-4" />
+                  <h1 className="text-xl">{i.symbol}</h1>
+                  <div className="flex justify-center gap-2">
+                    <a className="hover:cursor-pointer" href={i.websiteUrl}>
+                      <MdOutlineRequestPage />
+                    </a>
+                    <a className="hover:cursor-pointer" href={i.twitterUrl}>
+                      <FaTwitterSquare />
+                    </a>
+                  </div>
+                </div>
+                <div className="flex justify-start gap-2 align-middle">
+                  <div>Price: {i.price.toFixed(2)}</div>
+                  <div>
+                    {i.priceChange1w > 0 ? (
+                      <div className="text-green-500">
+                        {i.priceChange1w.toFixed(2)}
+                      </div>
+                    ) : (
+                      <div className="text-red-500">
+                        {i.priceChange1w.toFixed(2)}%
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div>Mcap: {(i.marketCap / 1000000000).toFixed(2)} b</div>
+                <div>Vol: {(i.volume / 1000000000).toFixed(2)} b</div>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
       <Footer />
